24f198139a0996fa803794d150881b364984c04e
Dugal Harris <dugalh@gmail.com>
2022-02-24 15:24:57 +0200

    scatter plots of NDVI vs AGC before and after homogenisation

--- scripts/gef_case_study.py
+++ scripts/gef_case_study.py
@@ -23,6 +23,7 @@ import numpy as np
 import geopandas as gpd, pandas as pd
 from sklearn import linear_model
 from matplotlib import pyplot
+from scipy import stats as stats
 
 import joblib, pickle
 
@@ -33,14 +34,20 @@ from map_thicket_agc import get_logger
 import pathlib
 ## extract features from multi-spectral satellite image
 plot_agc_shapefile_name = root_path.joinpath(r'data/outputs/geospatial/gef_plot_polygons_with_agc_v2.shp')
-# image_filename = root_path.joinpath(r'data/inputs/imagery/WorldView3_Oct2017_OrthoNgiDem_AtcorSrtmAdjCorr_PanAndPandSharpMs.tif')
-# image_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Source\NGI_3323DA_2015_GefSite_Source.vrt")
 # source_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Reference\COPERNICUS-S2-20151023T081112_20151023T081949_T34HGH_B4328.tif")
+# source_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Reference\LANDSAT-LC08-C02-T1_L2-LC08_172083_20150525_B4325.tif")
 source_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Source\NGI_3323DA_2015_GefSite_Source.vrt")
-# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k5_5.vrt")
+
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Reference\COPERNICUS-S2-20151023T081112_20151023T081949_T34HGH_B4328.tif")
+homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k5_5.vrt")
 # homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\Landsat\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k5_5.vrt")
 # homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k7_7.vrt")
-homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k11_11.vrt")
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k11_11.vrt")
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-BLK-OFFSET_k1_1.vrt")
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN_k5_5.vrt")
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\COPERNICUS-S2-20151023T081112_20151023T081949_T34HGH_B4328_HOMO_cREF_mGAIN-BLK-OFFSET_k1_1.tif")
+# homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN-OFFSET_k15_15.vrt")
+homo_filename = pathlib.Path(r"V:\Data\HomonimEgs\NGI_3323D_2015_GefSite\Homogenised\NGI_3323D_2015_GefSite_RGBN_HOMO_cREF_mGAIN_k15_15.vrt")
 
 logger = get_logger(__name__)
 logger.info('Starting...')
@@ -60,30 +67,46 @@ for gdf in [source_gdf, homo_gdf]:
     gdf.loc[gdf['data']['Stratum'] == 'Intact', ('data', 'Stratum')] = 'Pristine'
 
 # make an example scatter plot of feature vs AGC/ABC
-if False:
-    pyplot.rcParams["font.family"] = "arial"
-    pyplot.rcParams["font.size"] = "12"
-    pyplot.rcParams["font.style"] = "normal"
-    pyplot.rcParams['legend.fontsize'] = 'medium'
-    pyplot.rcParams['figure.titlesize'] = 'medium'
+pyplot.rcParams["font.family"] = "arial"
+pyplot.rcParams["font.size"] = "12"
+pyplot.rcParams["font.style"] = "normal"
+pyplot.rcParams['legend.fontsize'] = 'medium'
+pyplot.rcParams['figure.titlesize'] = 'medium'
+
+pyplot.figure()
+vis.scatter_ds(homo_gdf, x_col=('feats', 'log(mean(NDVI))'), y_col=('data', 'AgcHa'), class_col=('data', 'Stratum'),
+               xfn=lambda x: x, do_regress=True, label_col=('data', 'ID'))
+
+
+def plot_agc_corr(x, y, x_label='NDVI', y_label='AGC (t C ha$^{-1}$)'):
+    xlim = [np.nanmin(x), np.nanmax(x)]
+    ylim = [np.nanmin(y), np.nanmax(y)]
+    xd = np.diff(xlim)[0]
+    yd = np.diff(ylim)[0]
+
+    pyplot.axis('tight')
+    pyplot.axis(xlim + ylim)
+    pyplot.plot(x, y, marker='.', linestyle='None', markersize=5)
+
+    if True:
+        (slope, intercept, r, p, stde) = stats.linregress(x, y)
+        pyplot.text((xlim[0] + xd * 0.7), (ylim[0] + yd * 0.05), '$R^2$ = {0:.3f}'.format(r ** 2),
+                    fontdict={'size': 12})
+        yr = np.array(xlim) * slope + intercept
+        pyplot.plot(xlim, yr, 'k--', lw=2, zorder=-1)
+
+    pyplot.xlabel(x_label, fontdict={'size': 12})
+    pyplot.ylabel(y_label, fontdict={'size': 12})
+    # pyplot.axis('tight')
+
 
 labels = ['Source', 'Homogenised']
-for label, gdf in zip(labels, [source_gdf, homo_gdf]):
-    pyplot.figure()
-    vis.scatter_ds(gdf, x_col=('feats', 'log(mean(pan/R))'), y_col=('data', 'AgcHa'), class_col=('data', 'Stratum'),
-                   xfn=lambda x: x, do_regress=True)
-    pyplot.title(label)
-    pyplot.figure()
-    vis.scatter_ds(gdf, x_col=('feats', 'log(mean(NDVI))'), y_col=('data', 'AgcHa'), class_col=('data', 'Stratum'),
-                   xfn=lambda x: x, do_regress=True)
-    pyplot.title(label)
+fig = pyplot.figure()
+fig.set_size_inches(10, 4.5, forward=True)
+
+for i, gdf in enumerate([source_gdf, homo_gdf]):
+    pyplot.subplot(1, 2, i+1)
+    plot_agc_corr(gdf[('feats', '(mean(NDVI))')], gdf[('data', 'AgcHa')]/1000)
+    pyplot.title(labels[i])
 
 
-# mask = ~np.isnan(im_plot_agc_gdf['feats']['log(mean(pan/R))'])
-# im_plot_agc_gdf = im_plot_agc_gdf.loc[mask]
-#
-# pyplot.figure()
-# pyplot.plot(im_plot_agc_gdf[('feats', 'log(mean(pan/R))')][mask], im_plot_agc_gdf[('data', 'AgcHa')][mask] , '.')
-# np.corrcoef(im_plot_agc_gdf[('feats', 'log(mean(pan/R))')][mask],  im_plot_agc_gdf[('data', 'AgcHa')][mask])**2
-# from scipy import stats as stats
-# (slope, intercept, r, p, stde) = stats.linregress(im_plot_agc_gdf[('feats', 'log(mean(pan/R))')][mask], im_plot_agc_gdf[('data', 'AgcHa')][mask])
